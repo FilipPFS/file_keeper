85cb0c034c90bfaca684f75d50c151dbeb33f4e3
Filip Petrovic <filippetrovicfr04@gmail.com>
2024-11-17 20:23:24 +0100

    Fix

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -12,7 +12,9 @@ export const createSessionClient = async () => {
 
   const session = (await cookies()).get("appwrite-session");
 
-  if (!session || !session.value) throw new Error("No session.");
+  if (!session || !session.value) {
+    redirect("/sign-in");
+  }
 
   client.setSession(session.value);
 
