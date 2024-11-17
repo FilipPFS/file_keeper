c5534d75f1eb89cbb59f99d3cf827c6dc84020d0
Filip Petrovic <filippetrovicfr04@gmail.com>
2024-11-17 20:11:16 +0100

    Potential Fix on deployment

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -96,19 +96,23 @@ export const verifySecret = async ({
 };
 
 export const fetchCurrentUser = async () => {
-  const { databases, account } = await createSessionClient();
+  try {
+    const { databases, account } = await createSessionClient();
 
-  const result = await account.get();
+    const result = await account.get();
 
-  const user = await databases.listDocuments(
-    appwriteConfig.databaseId,
-    appwriteConfig.usersCollectionId,
-    [Query.equal("accountId", result.$id)]
-  );
+    const user = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      [Query.equal("accountId", result.$id)]
+    );
 
-  if (user.total <= 0) return null;
+    if (user.total <= 0) return null;
 
-  return parseStringify(user.documents[0]);
+    return parseStringify(user.documents[0]);
+  } catch (error) {
+    handleError(error, "Error fetching user.");
+  }
 };
 
 export const signOutUser = async () => {
